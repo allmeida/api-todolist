053e0eb96f49f399f30727017ec97e2fd03f0cb6
allmeida <allmeida.fran@gmail.com>
2021-01-03 20:07:09 -0400

    Criando TodoSeeder e dados fakers para cada usuário

--- app/User.php
+++ app/User.php
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @return HasMany
+     */
+    public function todos()
+    {
+        return $this->hasMany(Todo::class);
+    }
 }
